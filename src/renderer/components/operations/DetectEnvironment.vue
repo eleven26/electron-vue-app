60cdecc05bfeca11042401d1d5b4eed60aa9b785
ruby <916809498@qq.com>
2019-05-13 11:47:23 +0800

    detect environment

--- src/renderer/components/operations/DetectEnvironment.vue
+++ src/renderer/components/operations/DetectEnvironment.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
     <p>PHP 版本: {{php_version}}</p>
+    <p>swoole 版本: {{swoole_version}}</p>
   </div>
 </template>
 
 <script>
+  import { execute } from '../../commands/command'
+
   export default {
     name: 'DetectEnvironment',
 
     data () {
       return {
-        php_version: ''
+        php_version: this.phpVersion(),
+        swoole_version: this.swooleVersion()
+      }
+    },
+
+    methods: {
+      phpVersion () {
+        execute(`php -v`, output => {
+          this.php_version = output.match(/PHP (\d+\.\d+\.\d+)/)[1]
+        })
+      },
+      swooleVersion () {
+        execute(`php -r "echo phpversion('swoole');"`, output => {
+          this.swoole_version = output || '未安装'
+        })
       }
     }
   }
@@ -19,4 +36,3 @@
 <style scoped>
 
 </style>
-
