276a088189b86ff55dae2d07919467720c76e82b
ruby <916809498@qq.com>
2019-05-21 17:56:16 +0800

    Hide menus in windows

--- src/renderer/components/Menus.vue
+++ src/renderer/components/Menus.vue
@@ -14,50 +14,58 @@
 </template>
 
 <script>
+  import {isDebug, isWin} from '../utils'
+
   export default {
     name: 'Menus',
 
     data () {
+      let menus = [
+        {
+          title: '系统信息',
+          name: 'system-info'
+        },
+        {
+          title: '切换到某一分支',
+          name: 'switch-branch'
+        },
+        {
+          title: '拉取所有模块',
+          name: 'pull'
+        },
+        {
+          title: '切换 env',
+          name: 'switch-env'
+        },
+        // {
+        //   title: 'test',
+        //   name: 'test'
+        // },
+        // ,
+        // {
+        //   title: 'todo',
+        //   name: 'todo'
+        // },
+        {
+          title: 'Artisan 命令',
+          name: 'artisan'
+        },
+        {
+          title: 'Api Blame',
+          name: 'api-blame'
+        },
+        {
+          title: '注意事项',
+          name: 'notice'
+        }
+      ]
+      if (isWin() && !isDebug()) {
+        menus.filter(v => {
+          return v.name !== 'artisan' || v.name !== 'api-blame'
+        })
+      }
       return {
-        menus: [
-          {
-            title: '系统信息',
-            name: 'system-info'
-          },
-          {
-            title: '切换到某一分支',
-            name: 'switch-branch'
-          },
-          {
-            title: '拉取所有模块',
-            name: 'pull'
-          },
-          {
-            title: '切换 env',
-            name: 'switch-env'
-          },
-          // {
-          //   title: 'test',
-          //   name: 'test'
-          // },
-          // ,
-          // {
-          //   title: 'todo',
-          //   name: 'todo'
-          // },
-          {
-            title: 'Artisan 命令',
-            name: 'artisan'
-          },
-          {
-            title: 'Api Blame',
-            name: 'api-blame'
-          },
-          {
-            title: '注意事项',
-            name: 'notice'
-          }
-        ]
+        menus
       }
     },
 
