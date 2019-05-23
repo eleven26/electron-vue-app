e142688448ed28eaac5bb0fc4ce4df21288e3c79
ruby <916809498@qq.com>
2019-05-23 23:24:03 +0800

    add comment

--- src/renderer/utils/httpServer.js
+++ src/renderer/utils/httpServer.js
@@ -2,6 +2,9 @@ import store from '../store'
 import {execute} from '../commands'
 const vagrantPrefix = 'cd /Users/ruby/Homestead && vagrant ssh -- -t '
 
+// 使用  node 的 exec 执行启动 http server 的命令
+// 后续命令的执行通过 http server 执行 vagrant 虚拟机里面的命令
+
 function changeExec () {
   store.dispatch('changeExec', require('child_process').exec)
 }
