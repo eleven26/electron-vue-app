0ea044b0e1f5e162b2de3aa6c4aaefa188926288
ruby <916809498@qq.com>
2019-05-23 22:29:21 +0800

    Use swoole http server instead of CodeIgniter

--- src/renderer/utils/request.js
+++ src/renderer/utils/request.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const host = 'http://192.168.10.10:9304'
-const commandUri = '/index.php/command'
 
 function exec (command, options = null, callback = null) {
   if (typeof options === 'function') {
@@ -9,7 +8,7 @@ function exec (command, options = null, callback = null) {
   }
   axios({
     method: 'post',
-    url: host + commandUri,
+    url: host,
     data: { command }
   }).then(res => {
     callback(null, res.data, null)
